f2a864eeffc21c56de8e629e4739544cddd1ad87
samini91 <samini91@gmail.com>
2020-07-26 14:08:28 -0400

    stuff

--- dev/dotnet.nix
+++ dev/dotnet.nix
@@ -1,11 +1,56 @@
-{ pkgs, ... }:
+{ pkgs
+, stdenv
+, fetchurl
+, mono5
+, makeWrapper,
+  ... }:
 
+let
+  
+  omnisharp = stdenv.mkDerivation rec {
+
+    pname = "omnisharp-roslyn";
+    version = "1.35.1";
+
+    src = fetchurl {
+      url = "https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v${version}/omnisharp-mono.tar.gz";
+      sha256 = "0gx87qc9r3lhqn6q95y74z67sjcxnazkkdi9zswmaqyvjn8x7vf4";
+    };
+
+    nativeBuildInputs = [ makeWrapper ];
+
+    preUnpack = ''
+    mkdir src
+    cd src
+    sourceRoot=.
+  '';
+
+    installPhase = ''
+    mkdir -p $out/bin
+    cd ..
+		cp -r src $out/
+    ls -al $out/src
+    makeWrapper ${mono5}/bin/mono $out/bin/omnisharp \
+    --add-flags "$out/src/OmniSharp.exe"
+  '';
+
+    meta = with stdenv.lib; {
+      description = "OmniSharp based on roslyn workspaces";
+      homepage = "https://github.com/OmniSharp/omnisharp-roslyn";
+      platforms = platforms.linux;
+      license = licenses.mit;
+      maintainers = with maintainers; [ tesq0 ];
+    };
+
+  };
+in
 {
   environment.systemPackages = with pkgs; [
     vscode
     dotnetCorePackages.sdk_3_1
-  	dotnetCorePackages.netcore_3_1
+    dotnetCorePackages.netcore_3_1
     dotnetCorePackages.aspnetcore_3_1
     mono6
+    omnisharp
   ];
 }
