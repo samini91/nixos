a0b675fc8a71db4333f6b3f0ce0df192bbf6085a
samini91 <samini91@gmail.com>
2020-08-13 04:00:21 +0000

    compton conf

--- usr/gorgeous.nix
+++ usr/gorgeous.nix
@@ -31,6 +31,7 @@
 
     home.file.".xmonad/xmonad.hs".source = ./config/xmonad/xmonad.hs;
     home.file.".Xmodmap".source = ./config/.Xmodmap;
+    home.file."compton/compton.conf".source = ./config/compton/compton.conf;
     
 
 
