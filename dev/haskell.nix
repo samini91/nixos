71944f9553938b1a6b9d3527fa5e342d50ff9c60
samini91 <samini91@gmail.com>
2021-05-15 04:39:28 +0000

    updated haskell language server

--- dev/haskell.nix
+++ dev/haskell.nix
@@ -1,5 +1,10 @@
 { pkgs ? import <nixpkgs> {}, ... }:
 
+let
+  # update this hash for newer versions of unstable stuff
+  haskell883 = import
+    (builtins.fetchTarball https://github.com/nixos/nixpkgs/tarball/a52e974cff8fb80c427e0d55c01b3b8c770ccec4){};
+in 
   with pkgs; with haskellPackages; [
     ghc
     ghcid
@@ -8,6 +13,7 @@
     postgresql-libpq
     implicit-hie
     ##haskell-language-server
-    haskell.packages.ghc883.haskell-language-server ## this for somereason takes forever
+    #haskell883.haskell.packages.ghc883.haskell-language-server ## this for somereason takes forever
+    haskell-language-server
   ]
 
