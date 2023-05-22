dd52e4af9484a93212ea73a1b55a5f12b25c7665
samini91 <samini91@gmail.com>
2023-05-22 15:27:50 +0000

    temp nix develop

--- hosts/nixshell/shell.nix
+++ hosts/nixshell/shell.nix
@@ -1,23 +1,21 @@
-# To be Deleted in lieu of nix develop
-{ pkgs ? import <nixpkgs> { config.allowUnfree = true; } , ... }:
+{ pkgs , ... }:
 
-
-  pkgs.mkShell {
-    buildInputs = with pkgs; [      
-
-    ]
-    ++ import ../../dev/emacs.nix pkgs
-    ++ import ../../dev/dotnet.nix { }
-    ++ import ../../dev/go/go.nix { }
-    ++ import ../../dev/java/java.nix { }
-    ++ import ../../dev/nix.nix { }  
-    ++ import ../../dev/haskell.nix { }
-    ++ import ../../dev/rust.nix { }
-    ++ import ../../dev/javascript/typescript.nix { }
-    ++ import ../../dev/scala/scala.nix { }
-    ++ import ../../dev/python/python.nix { }
-    ++ import ../../dev/devtools/devtools.nix { }
-    ++ import ../../dev/database/postgres.nix { } ;
-  }
+pkgs.mkShell {
+  imports = [
+    
+  ] ;
+  packages = with pkgs; [      
+    emacs
+    ripgrep
+    git
+    gnumake
+    sshfs
+    wget
+    cmake
+    libtool
+    libvterm
+    minikube
+  ] ;
+}
 
 
