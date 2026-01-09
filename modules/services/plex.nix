c0c253b59f0d4e421d91454631246bcc8502cabe
samini91 <samini91@gmail.com>
2026-01-09 03:44:16 +0000

    stuff

--- modules/services/plex.nix
+++ modules/services/plex.nix
@@ -1,16 +1,18 @@
 { config, options, pkgs, lib, ... }:
-
 with pkgs;
 with lib;
+
 let serviceCfg = config.modules.services;
     cfg = serviceCfg.plex;
 in
 {
-  config.modules.services.plex.enable = mkEnableOption "plex";  
+  options.modules.services.plex.enable = mkEnableOption "Plex";  
 
   config.services = mkIf cfg.enable {
-    plex.enable = true;
-    plex.openFirewall = true;
+    plex = {
+      enable = true;
+      openFirewall = true;
+    };
   };
 }
 
